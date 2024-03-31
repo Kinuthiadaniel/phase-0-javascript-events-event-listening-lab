d2622aea2f0de0d00d1c8601548343c7f27fa92f
Kinuthiadaniel <dankinuthia17@gmail.com>
2024-03-31 09:06:14 +0300

    intial commit

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+const input = document.getElementById("button");
+function clickAlert(){
+  alert("Who is clicking me?!")
 }
+const divide = document.getElementById("main");
+divide.addEventListener("click", clickAlert)
+input.addEventListener("click", clickAlert)
+}
+addingEventListener();
